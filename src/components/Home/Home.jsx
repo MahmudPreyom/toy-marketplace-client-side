a4de7a670c2fd3ef65d648f7c15bfac2aa747065
MahmudPreyom <preyom5076@gmail.com>
2023-05-18 12:00:30 +0600

    navbar add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Footer from "../Shared/Footer";
 
 const Home = () => {
     return (
         <div>
             <h2>This is Home</h2>
-            <Footer></Footer>
         </div>
     );
 };
